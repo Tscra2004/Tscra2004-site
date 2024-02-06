73f9534b3025ac20a150e6662e5e86693aeed7e6
Tscra2004 <tscra2004@gmail.com>
2024-02-06 14:28:13 +0100

    commit_innit

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+</head>
+<body>
+    <?php 
+          echo "C'est un script PHP!\n"; 
+        ?>
+</body>
+</html>
